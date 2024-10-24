bc273cba53562307906784b9a881d7f73dc3f587
Minto312 <karinto3219@gmail.com>
2024-10-24 18:07:09 +0000

    [add] polliing reception

--- reception/src/hooks/useReceptions.ts
+++ reception/src/hooks/useReceptions.ts
@@ -6,7 +6,11 @@ import useSWR from 'swr';
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export const useReceptions = () => {
-  const { data: receptions, isLoading, error, mutate } = useSWR<Reception[]>('/api/reception', fetcher);
+  const { data: receptions, error, mutate } = useSWR<Reception[]>('/api/reception', fetcher, {
+    refreshInterval: 5000, // 5秒ごとにデータを再取得
+  });
+
+  const isLoading = !receptions && !error;
 
   const refetch = () => {
     mutate();
